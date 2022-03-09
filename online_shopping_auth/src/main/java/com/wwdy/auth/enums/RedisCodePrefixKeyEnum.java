33b2bcbad250e63ba9e0be7c33c4cbb5b6d6ff99
WWDY <6618@tom.com>
2022-03-09 11:30:00 +0800

    [add] 添加验证码登功能

---
+++ online_shopping_auth/src/main/java/com/wwdy/auth/enums/RedisCodePrefixKeyEnum.java
@@ -0,0 +1,6 @@
+package com.wwdy.auth.enums;/**
+ * @author  wwdy
+ * @date  2022/3/9 10:14
+ */
+public enum codeTypeEnum {
+}
---
+++ online_shopping_auth/src/main/java/com/wwdy/auth/pojo/dto/LoginByPhoneDTO.java
@@ -0,0 +1,6 @@
+package com.wwdy.auth.pojo.dto;/**
+ * @author  wwdy
+ * @date  2022/3/9 9:58
+ */
+public class LoginByPhoneDTO {
+}
